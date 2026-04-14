b616f7b7b74d07e81371e218475ffd9f419c66c0
Dmitry <Dololob92@gmail.com>
2026-04-14 22:01:16 +0300

    feat: avatar animation added

--- cv-react/src/components/header/Avatar.tsx
+++ cv-react/src/components/header/Avatar.tsx
@@ -1,33 +1,41 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "./Header.module.css";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export const Avatar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const SwiperElem = useCallback(() => {
+    return (
+      <Swiper
+        cssMode={true}
+        className={styles.slider}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <SwiperSlide className={styles.sliderSwapper}>
+          <img
+            src="../../../assets/avatar.jpg"
+            alt="avatar"
+            className={styles.sliderAvatar}
+          />
+        </SwiperSlide>
+      </Swiper>
+    );
+  }, [isOpen]);
+
   return (
     <>
+      <div className={styles.oval1}></div>
+      <div className={styles.oval2}></div>
+      <div className={styles.oval3}></div>
+      <div className={styles.oval4}></div>
       <img
         onClick={() => setIsOpen(!isOpen)}
         src="../../../assets/avatar.jpg"
         alt="avatar"
         className={styles.avatar}
       />
-      {isOpen && (
-        <Swiper
-          cssMode={true}
-          className={styles.slider}
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <SwiperSlide className={styles.sliderSwapper}>
-            <img
-              src="../../../assets/avatar.jpg"
-              alt="avatar"
-              className={styles.sliderAvatar}
-            />
-          </SwiperSlide>
-        </Swiper>
-      )}
+      {isOpen && <SwiperElem />}
     </>
   );
 };
